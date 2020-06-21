40228194df56e8e6660c7d1c2a7d8258942673ed
Francis Njuguna <coulsorfrancois@gmail.com>
2020-06-21 09:52:09 +0300

    Create the ShowProducts.component.js

---
+++ src/components/ShowProducts.component.js
@@ -0,0 +1,16 @@
+import React, {Component} from 'react';
+
+
+class ShowProducts extends Component {
+
+    render(){
+        
+        <div>
+            
+        </div>
+    }
+
+    componentDidMount(){
+
+    }
+}
